80ceb074a970c41b9021a7fa0f2d134813258914
Benoit Masson-Bedeau <benoit.masson-bedeau@outlook.com>
2018-04-15 20:10:12 +0200

    Chapter 12: beginning for Alien Invasion.

---
+++ alien_invasion/alien_invasion/settings.py
@@ -0,0 +1,20 @@
+class Settings():
+    """ A class to store all settings for Alien invasion. """
+
+    def __init__(self):
+        """ Initialise the game's settings. """
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        #self.bg_color = (0, 0, 255)
+
+        # Ship settings.
+        self.ship_speed_factor = 1.5
+
+        # Bullet settings.
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
